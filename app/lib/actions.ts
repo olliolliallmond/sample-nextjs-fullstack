2d9a6d9fd07540643e2d0ac79d4e77a0ce3adb4c
Amberlee Allmond <aallmondcar@gmail.com>
2024-04-25 19:46:51 -0400

    validate and prep data to send to db

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,33 @@
 'use server';
+
+import { z } from 'zod'; // TS validation library
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({id: true, date: true})
  
 export async function createInvoice(formData: FormData) {
-  // const rawFormData = {
-  //   customerId: formData.get('customerId'),
-  //   amount: formData.get('amount'),
-  //   status: formData.get('status'),
-  // };
-  const rawFormData = Object.fromEntries(formData.entries())
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  // store in cents to avoind JS floating-point errors and to improve accuracy
+  const amountInCents = amount * 100; 
+
+  // capture date, split at 'T' to keep date and exclude time
+  const date = new Date().toISOString().split('T')[0]
+  console.log("date: " + date)
+
+  // const rawFormData = Object.fromEntries(formData.entries())
+
   // Console log visible in dev terminal running app
-  console.log(rawFormData);
+  // console.log(rawFormData);
 }
