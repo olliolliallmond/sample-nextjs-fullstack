17f57ed6fbb08e47f4840301ea71bbf4693794b7
Amberlee Allmond <aallmondcar@gmail.com>
2024-04-25 19:34:57 -0400

    Update server action to extract data from formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,12 @@
 'use server';
  
-export async function createInvoice(formData: FormData) {}
+export async function createInvoice(formData: FormData) {
+  // const rawFormData = {
+  //   customerId: formData.get('customerId'),
+  //   amount: formData.get('amount'),
+  //   status: formData.get('status'),
+  // };
+  const rawFormData = Object.fromEntries(formData.entries())
+  // Console log visible in dev terminal running app
+  console.log(rawFormData);
+}
