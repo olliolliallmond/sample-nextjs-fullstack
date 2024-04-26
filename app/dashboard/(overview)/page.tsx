41e60074215e2e23d4ad09f8157cf3aebf0a6b91
Amberlee Allmond <aallmondcar@gmail.com>
2024-04-25 21:53:59 -0400

    removed dead code

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,21 +1,11 @@
-// import { Card } from '@/app/ui/dashboard/cards';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {  fetchLatestInvoices, fetchCardData } from '@/app/lib/data'; // removed fetchRevenue
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, InvoiceSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  // Request Waterfall
-  // const revenue = await fetchRevenue();
-  // const latestInvoices = await fetchLatestInvoices();
-  // const {numberOfCustomers,
-  //     numberOfInvoices,
-  //     totalPaidInvoices,
-  //     totalPendingInvoices} = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -25,10 +15,6 @@ export default async function Page() {
                 <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card title="Total Customers" value={numberOfCustomers} type="customers" /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
