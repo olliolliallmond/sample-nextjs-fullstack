2fad11dcd23901ceeb64367270595794175fede6
Amberlee Allmond <aallmondcar@gmail.com>
2024-04-25 22:14:46 -0400

    readibility, dead code

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,7 +15,6 @@ export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
-
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
@@ -36,7 +35,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
-
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -58,7 +56,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   noStore();
-
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -70,7 +67,6 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
-    // Parallel query
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
@@ -100,7 +96,6 @@ export async function fetchFilteredInvoices(
   currentPage: number,
 ) {
   noStore();
-
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -134,7 +129,6 @@ export async function fetchFilteredInvoices(
 
 export async function fetchInvoicesPages(query: string) {
   noStore();
-
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -157,7 +151,6 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
-
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -202,7 +195,6 @@ export async function fetchCustomers() {
 
 export async function fetchFilteredCustomers(query: string) {
   noStore();
-  
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
