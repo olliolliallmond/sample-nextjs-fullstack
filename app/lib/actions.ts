919ded1f4482f5dbfc9c6f7d32b8adbc4db11999
Amberlee Allmond <aallmondcar@gmail.com>
2024-04-25 20:41:01 -0400

    improve accessibility and validation of 'create invoice' form and form actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,19 +7,50 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+           .number()
+           .gt(0, { message: 'Please enter an amount greater that $0.'}),
+  status: z.enum(['pending', 'paid'],{
+    invalid_type_error: 'Please select an invoice status.'
+  }),
   date: z.string(),
 })
 // Create
 const CreateInvoice = FormSchema.omit({id: true, date: true})
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+// export async function createInvoice(formData: FormData) {
+  // const { customerId, amount, status } = CreateInvoice.parse({
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
 
   // store in cents to avoind JS floating-point errors and to improve accuracy
   const amountInCents = amount * 100; 
